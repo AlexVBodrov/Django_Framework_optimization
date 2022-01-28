8b780b7fb492ee90d4be71bcaada231dceee27f4
AlexVBodrov <alexvbodrov@mail.ru>
2022-01-28 04:25:40 +0300

    add send_verify_mail in views add settings in settings.py send mail Debug.Server

--- authnapp/urls.py
+++ authnapp/urls.py
@@ -1,8 +1,11 @@
 # from django.urls import path
 from django.urls import re_path
-import authnapp.views as authnapp
 
-app_name = "authnapp"
+import authnapp.views as authnapp
+from authnapp.apps import AuthnappConfig
+
+app_name = AuthnappConfig.nameapp_name = "authnapp"
+
 #
 # urlpatterns = [
 #     path("login/", authnapp.login, name="login"),
@@ -17,4 +20,5 @@ urlpatterns = [
     re_path(r"^logout/$", authnapp.logout, name="logout"),
     re_path(r"^register/$", authnapp.register, name="register"),
     re_path(r"^edit/$", authnapp.edit, name="edit"),
+    re_path(r"^verify/(?P<email>.+)/(?P<activation_key>\w+)/$", authnapp.verify, name="verify"),
 ]
